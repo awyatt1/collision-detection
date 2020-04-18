349c7908bef5cafa3c97bf8e589a9b629342f8a3
awyatt1 <62950453+awyatt1@users.noreply.github.com>
2020-04-18 12:14:26 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,23 @@
-var rectangle, movingRectangle;
+var fixedRect, movingRect;
 
 function setup() {
-  createCanvas(800,400);
-  rectangle = createSprite(200, 200, 50, 80);
-  movingRectangle = createSprite(240,200,40,60);
-  rectangle.shapeColor = "green";
-  movingRectangle.shapeColor = "green";
-  movingRectangle.debug = true;
-  rectangle.debug = true;
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(0,0,0);
- movingRectangle.x = World.mouseX;
- movingRectangle.y = World.mouseY;
- if(movingRectangle.x - rectangle.x < rectangle.width/2 + movingRectangle.width/2 
-  && rectangle.x - movingRectangle.x < rectangle.width/2 + movingRectangle.width/2
-  && movingRectangle.y - rectangle.y < rectangle.height/2 + movingRectangle.height/2
-  && rectangle.y - movingRectangle.y < rectangle.height/2 + movingRectangle.height/2){
-   movingRectangle.shapeColor = "red";
-   rectangle.shapeColor = "red";
- }
- else{
-   movingRectangle.shapeColor ="green";
-   rectangle.shapeColor = "green";
- }
+  background(0,0,0);  
+  bounceOff(movingRect, fixedRect);
   drawSprites();
 }
+
+
+
